0c60553c6744b84dfdf79e3a84e805e3bbc1c590
André Bonnano <98353333+bonnano1980@users.noreply.github.com>
2022-03-19 11:44:07 -0300

    Start implement tabuleiro

--- JogoXadrezConsole/Program.cs
+++ JogoXadrezConsole/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 
 namespace JogoXadrezConsole
@@ -8,8 +8,7 @@ namespace JogoXadrezConsole
     {
         static void Main(string[] args)
         {
-            Posicao P = new Posicao(3, 4);
-            Console.WriteLine("Posição: " + P);
+            Tabuleiro tab = new Tabuleiro(8,8);
         }
     }
 }
